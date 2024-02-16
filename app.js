136c8b5c5d31d5c040e36dc0c6b19c11fc519a4e
Musaad <musaadalsh3@gmail.com>
2024-02-16 04:12:30 +0300

    adding more colors & centering button

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const but = document.querySelector('button')
+let count = 0
+const colors = ['red', 'grey', 'black']
 but.addEventListener('click', () => {
-    document.body.style.backgroundColor = 'red'
+    document.body.style.backgroundColor = colors[count % colors.length]
+    count++
 })
